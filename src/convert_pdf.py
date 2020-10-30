4f3b67b40bdf31715975708634c9342d604fbf08
Romez <Romez1990@gmail.com>
2020-10-30 19:57:34 +0300

    feat(convert_pdf): create convert pdf to images function

---
+++ src/convert_pdf.py
@@ -0,0 +1,24 @@
+from io import BytesIO
+from pathlib import Path
+from typing import (
+    List,
+)
+from pdf2image import convert_from_path
+from PIL import Image
+
+from .paths import poppler_path
+
+
+def convert_pdf_to_images(path: Path) -> List[bytes]:
+    pages = convert_from_path(str(path),
+                              poppler_path=poppler_path)
+    pages_bytes = []
+    for page in pages:
+        pages_bytes.append(convert_image_to_bytes(page))
+    return pages_bytes
+
+
+def convert_image_to_bytes(image: Image) -> bytes:
+    bytes_io = BytesIO()
+    image.save(bytes_io, 'JPEG')
+    return bytes_io.getvalue()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+from pathlib import Path
+
+from .convert_pdf import convert_pdf_to_images
+from .paths import (
+    project_path,
+    document_samples_path,
+)
+
+
 def main() -> None:
-    pass
+    pages = convert_pdf_to_images(document_samples_path / 'original.pdf')
+    for number, page in enumerate(pages, 1):
+        save(page, project_path / 'output' / f'page {number}.jpg')
+
+
+def save(image: bytes, file_path: Path) -> None:
+    directory = file_path.parent
+    directory.mkdir(parents=True, exist_ok=True)
+    with open(file_path, 'wb') as file:
+        file.write(image)
 
 
 main()
